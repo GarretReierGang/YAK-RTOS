5f7efdc91627bcef2e3a107a2486ed0cbd7b3f48
GarretReierGang <GarretReierGang@gmail.com>
2021-03-03 17:00:21 -0700

    Added lab5/lab6 files

---
+++ lab5inth.c
@@ -0,0 +1,39 @@
+#include "clib.h"
+#include "yakk.h"
+
+extern int KeyBuffer;
+extern YKSEM* NSemPtr;
+
+void reset_handler() {
+	exit(0);
+}
+
+void tick_handler() {
+	static int tick_num = 0;
+	tick_num++;
+	printNewLine();
+	printString("TICK ");
+	printInt(tick_num);
+	printNewLine();
+    YKTickHandler();
+}
+
+void keyboard_handler() {
+	int asdfasdf = 0;
+	if(KeyBuffer == 100) {
+		printNewLine();
+		printString("DELAY KEY PRESSED");
+		printNewLine();
+		while(asdfasdf < 5000)
+			asdfasdf++;
+
+		printNewLine();
+		printString("DELAY COMPLETE");
+		printNewLine();
+		return;
+	}
+    if (KeyBuffer == 'p')
+    {
+       YKSemPost(NSemPtr);
+    }
+}
---
+++ lab6isr.s
@@ -0,0 +1,64 @@
+reset:
+	mov ax, 0
+	push ax
+	call exit
+tick:
+	push AX
+	push BX
+	push CX
+	push DX
+	push BP
+	push SI
+	push DI
+	push DS
+	push ES
+
+    call YKEnterISR
+	sti
+	call lab6tick
+	cli
+    call YKExitISR
+
+	mov al, 0x20
+	out 0x20, al
+	pop ES
+	pop DS
+	pop DI
+	pop SI
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	iret
+keyboard:
+	push AX
+	push BX
+	push CX
+	push DX
+	push BP
+	push SI
+	push DI
+	push DS
+	push ES
+
+    call YKEnterISR
+
+	sti
+	call lab6keybrd
+	cli
+
+    call YKExitISR
+
+	mov al, 0x20
+	out 0x20, al
+	pop ES
+	pop DS
+	pop DI
+	pop SI
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	iret
